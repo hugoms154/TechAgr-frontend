139eb2e12c4bf7c46b9c73fb5d02e6c42717cd6d
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-26 20:01:49 -0300

    remove: unnecessary import

--- src/pages/CreatePost/index.tsx
+++ src/pages/CreatePost/index.tsx
@@ -1,5 +1,4 @@
 import React, { FormEvent, useCallback, useState } from 'react';
-import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 
