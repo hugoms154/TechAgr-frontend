52e8fefcc4860a93c773e9fff11f2b9a9438be47
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-26 18:37:59 -0300

    Adding: Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import CreatePost from '../pages/CreatePost';
 import Dashboard from '../pages/Dashboard';
+import Post from '../pages/Post';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={Dashboard} />
+      <Route exact path="/posts/:id" component={Post} />
+      <Route exact path="/posts" component={CreatePost} />
     </Switch>
   );
 };
